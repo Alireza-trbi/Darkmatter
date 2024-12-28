b2c754e1cb9d14a5e8762f59b9f1cf5a87cab7e3
Alireza-trbi <72087681+Alireza-trbi@users.noreply.github.com>
2024-12-28 17:53:26 +0330

    Update mahsa.sh

--- mahsa.sh
+++ mahsa.sh
@@ -233,7 +233,7 @@ uci commit system
 
 sed -i 's/XTLS\/Xray-core/GFW-knocker\/Xray-core/g' /usr/lib/lua/luci/passwall2/com.lua
 
-uci set system.@system[0].hostname=By-AmirHossein
+uci set system.@system[0].hostname=By-ALIREZA_TRBI
 
 uci commit system
 
@@ -241,9 +241,12 @@ uci commit network
 
 uci commit wireless
 
-uci set dhcp.@dnsmasq[0].rebind_domain='www.ebanksepah.ir 
+uci set dhcp.@dnsmasq[0].rebind_domain='qmb.ir
+medu.ir
+ebanksepah.ir
+banksepah.ir
+medu.gov.ir
 my.irancell.ir'
-
 uci commit
 
 uci commit
@@ -252,7 +255,7 @@ rm /usr/bin/xray
 
 echo -e "${YELLOW}** Warning : To install Mahsa Core visit > Passwall2 > App Update > Xray Force Update ** ${ENDCOLOR}"
 
-echo -e "${MAGENTA} Made With Love By : AmirHossein ${ENDCOLOR}"
+echo -e "${MAGENTA} Made With By : ALIREZA_TRBI ${ENDCOLOR}"
 
 rm passwall2x.sh
 
